cfeb932b6dc54a117da71255b7fd96d82d100756
Alexxmfs <alexxmfs@hotmail.com>
2024-10-12 18:49:38 -0300

    cadastro de evento e listagem

---
+++ src/app/criar-evento/page.tsx
@@ -0,0 +1,217 @@
+  "use client";
+  import React, { useState, useEffect } from "react";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import axios from "axios";
+  import {
+    faChevronDown
+  } from "@fortawesome/free-solid-svg-icons";
+  import "./criarEvento.css";
+  import { useRouter } from "next/navigation"; // Importe o useRouter
+
+  interface Local {
+    id_local: number;
+    nome: string;
+  }
+
+  const CriarEvento = () => {
+    const router = useRouter(); // Inicialize o router
+
+    const [nome, setNome] = useState("");
+    const [dataEvento, setDataEvento] = useState("");
+    const [horarioEvento, setHorarioEvento] = useState("");
+    const [tipo, setTipo] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [locais, setLocais] = useState<Local[]>([]); // Estado para armazenar a lista de locais
+    const [selectedLocalId, setSelectedLocalId] = useState<number | null>(null); // Estado para armazenar o ID do local selecionado
+
+    useEffect(() => {
+      fetchLocais(); // Carrega a lista de locais ao iniciar o componente
+    }, []);
+
+    const fetchLocais = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/locais");
+        setLocais(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar locais:", error);
+      }
+    };
+
+    const handleSubmit = async (e: { preventDefault: () => void }) => {
+      e.preventDefault();
+
+      try {
+        await axios.post("http://localhost:5000/criar-eventos", {
+          nome,
+          data_evento: dataEvento,
+          horario_evento: horarioEvento,
+          tipo,
+          email,
+          telefone,
+          id_local: selectedLocalId, // Envia o ID do local selecionado
+        });
+
+        setNome("");
+        setDataEvento("");
+        setHorarioEvento("");
+        setTipo("");
+        setEmail("");
+        setTelefone("");
+        setSelectedLocalId(null); // Limpa o local selecionado
+
+        // alert("Evento e dados associados criados com sucesso!");
+        router.push("/eventos?success=true");
+      } catch (error) {
+        console.error(error);
+        alert("Erro ao criar evento e dados associados");
+      }
+    };
+
+    return (
+      <div className="bg-[#191E28] min-h-screen p-8">
+        <div className="max-w-3xl mx-auto">
+          <h4 className="text-gray-400 pb-8 pt-5 text-lg">
+            Home / <span className="text-[#6d99fb]">Eventos</span>
+          </h4>
+          <h2 className="text-3xl pb-2 text-white mb-2">Adicionar novo evento</h2>
+          <p className="text-gray-400 mb-6">*Campos obrigatórios</p>
+        </div>
+
+        <div className="max-w-3xl mx-auto bg-[#10141d] p-6 rounded-lg shadow-md">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <h4 className="text-lg text-gray-300 pb-2 pt-4">
+              Informações básicas
+            </h4>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <label className="block text-gray-300 mb-1">Nome do evento*</label>
+                <input
+                  type="text"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                  placeholder="Informe o nome do evento"
+                  className="w-full p-2 bg-gray-700 text-white rounded focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+
+              <div>
+                <label className="block text-gray-300 mb-1">
+                  Selecione um tipo*
+                </label>
+                <div className="relative">
+                  <select
+                    className="w-full p-2 bg-gray-700 text-white rounded appearance-none focus:outline-none focus:ring focus:ring-blue-500"
+                    value={tipo}
+                    onChange={(e) => setTipo(e.target.value)}
+                    required
+                  >
+                    <option value="">Selecione um tipo</option>
+                    <option value="show">Show</option>
+                    <option value="futebol">Futebol</option>
+                    <option value="feira">Feira</option>
+                  </select>
+                  <FontAwesomeIcon
+                    className="absolute right-2 top-3 text-gray-400 text-lg w-4"
+                    icon={faChevronDown}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label className="block text-gray-300 mb-1">Data do evento*</label>
+                <input
+                  type="date"
+                  value={dataEvento}
+                  onChange={(e) => setDataEvento(e.target.value)}
+                  className="w-full p-2 bg-gray-700 text-white rounded focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-300 mb-1">Horário do evento*</label>
+                <input
+                  type="time"
+                  value={horarioEvento}
+                  onChange={(e) => setHorarioEvento(e.target.value)}
+                  className="w-full p-2 bg-gray-700 text-white rounded focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="h-px bg-[#333B49] my-4" />
+
+            <h4 className="text-lg text-gray-300 pb-2 pt-4">Localização</h4>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <label className="block text-gray-300 mb-1">
+                  Selecione um local*
+                </label>
+                <div className="relative">
+                  <select
+                    className="w-full p-2 bg-gray-700 text-white rounded appearance-none focus:outline-none focus:ring focus:ring-blue-500"
+                    value={selectedLocalId ? selectedLocalId.toString() : ""} // Certifique-se de que o valor seja uma string
+                    onChange={(e) => setSelectedLocalId(parseInt(e.target.value))} // Converter o valor para número
+                    required
+                  >
+                    <option value="">Selecione um local</option>
+                    {locais.map((local) => (
+                      <option key={local.id_local} value={local.id_local}>
+                        {local.nome}
+                      </option>
+                    ))}
+                  </select>
+                  <FontAwesomeIcon
+                    className="absolute right-2 top-3 text-gray-400 text-lg w-4"
+                    icon={faChevronDown}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="h-px bg-[#333B49] my-4" />
+
+            <h4 className="text-lg text-gray-300 pb-2 pt-4">Contato</h4>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              <div>
+                <label className="block text-gray-300 mb-1">E-mail*</label>
+                <input
+                  type="email"
+                  placeholder="Informe o e-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full p-2 bg-gray-700 text-white rounded focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-gray-300 mb-1">Telefone*</label>
+                <input
+                  type="text"
+                  placeholder="Informe o telefone"
+                  value={telefone}
+                  onChange={(e) => setTelefone(e.target.value)}
+                  className="w-full p-2 bg-gray-700 text-white rounded focus:outline-none focus:ring focus:ring-blue-500"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-end space-x-4 pt-3">
+              <button
+                type="button"
+                className="px-8 py-2 bg-[#10141d] border border-[#EBF0F9] text-[#EBF0F9] text-lg rounded hover:bg-[#1c1f24] focus:outline-none"
+              >
+                Cancelar
+              </button>
+              <button
+                type="submit"
+                className="px-8 py-2 bg-[#EBF0F9] text-[#333B49] rounded hover:bg-[#d1e1f3] focus:outline-none font-semibold text-lg"
+              >
+                Cadastrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  export default CriarEvento;
--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,15 +1,317 @@
-import React from 'react'
-import "./evento.css"
+"use client";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faEllipsisV,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { toast, Toaster } from "react-hot-toast";
+import { useSearchParams, useRouter } from "next/navigation";
+import "./eventos.css";
+
+interface Evento {
+  id_evento: number;
+  nome: string;
+  data_evento: string;
+  horario_evento: string;
+  tipo: string;
+  id_local: number;
+  data_atualizacao: string;
+  local: {
+    nome: string; // Add the local name here
+    endereco: string; // Add the local's address here
+    portões: string; // Add the local's gates here
+  };
+}
 
 const Eventos = () => {
+  const [eventos, setEventos] = useState<Evento[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const searchParams = useSearchParams();
+  const success = searchParams.get("success");
+  const [selectedEvento, setSelectedEvento] = useState<number | null>(null);
+  const router = useRouter(); // Initialize useRouter
+
+  const toggleDropdown = (id: number) => {
+    setSelectedEvento(selectedEvento === id ? null : id);
+  };
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  useEffect(() => {
+    if (success && !showAlert) {
+      if (success === "true") { // Check the value of success
+        showSuccessToast("Um novo evento foi adicionado");
+      } else if (success === "edited") { // Check for "edited" value
+        showSuccessToast("uma nova edição foi salva");
+      }
+      setShowAlert(true); // Prevent showing toast multiple times
+  
+      // Remove 'success' from the URL after processing.  This allows showing the correct toast on page refresh.
+      const updatedSearchParams = new URLSearchParams(searchParams);
+      updatedSearchParams.delete("success");
+      router.replace(`?${updatedSearchParams.toString()}`);
+  
+    }
+  }, [success, showAlert, router, searchParams]);
+
+  useEffect(() => {
+    fetchEventos();
+  }, []);
+
+  const fetchEventos = () => {
+    axios
+      .get("http://localhost:5000/eventos") 
+      .then((response) => {
+        const data = Array.isArray(response.data) ? response.data : [];
+        setEventos(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar eventos:", error);
+      });
+  };
+
+  const showSuccessToast = (message: string) => { // Takes a message parameter
+    toast.success(
+      <div
+        style={{ display: "flex", alignItems: "center", marginRight: "75px" }}
+      >
+        <div>
+          <span style={{ fontSize: "18px", fontWeight: "600" }}>Sucesso</span>
+          <div
+            style={{ fontSize: "14px", color: "#ecf0f1", paddingTop: "4px" }}
+          >
+            {message} {/* Display the passed message */}
+          </div>
+        </div>
+      </div>,
+      {
+        duration: 3000,
+        position: "bottom-left",
+        style: {
+          backgroundColor: "#2F3B28",
+          color: "#ecf0f1",
+          borderLeft: "5px solid #99C766",
+          padding: "12px",
+          borderRadius: "8px",
+          display: "flex",
+          alignItems: "center",
+          width: "800px",
+        },
+        icon: (
+          <div
+            style={{
+              backgroundColor: "#99C766",
+              width: "28px",
+              height: "28px",
+              borderRadius: "50%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <span style={{ color: "#2F3B28", fontSize: "15px" }}>
+              <FontAwesomeIcon icon={faCheck} />
+            </span>
+          </div>
+        ),
+      }
+    );
+  };
+
+  const deleteEvento = (id_evento: number) => {
+    axios
+      .delete(`http://localhost:5000/deletar-eventos/${id_evento}`)
+      .then((response) => {
+        if (response.status === 200) {
+          toast.success("Evento deletado com sucesso!");
+          fetchEventos(); // Atualiza a lista de eventos após deletar
+        } else {
+          toast.error("Erro ao deletar o evento.");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao deletar evento:", error);
+        toast.error("Erro ao deletar o evento.");
+      });
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+
+    if (value) {
+      axios
+        .get(`http://localhost:5000/pesquisar-eventos?nome=${value}`)
+        .then((response) => {
+          const data = Array.isArray(response.data) ? response.data : [];
+          setEventos(data);
+        })
+        .catch((error) => {
+          console.error("Erro ao pesquisar eventos:", error);
+        });
+    } else {
+      fetchEventos(); // Se o campo de pesquisa estiver vazio, re-carrega todos os eventos
+    }
+  };
+
+  const totalPages = Math.ceil(eventos.length / itemsPerPage);
+
+  const changePage = (page: number) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  const handleEdit = (id_evento: number) => {
+    router.push(`/editar-evento?id=${id_evento}`); // Navigate to /editar-evento with the id as a query parameter
+  };
+
+  const displayedEventos = eventos.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
   return (
-    <div>
-      <h1>Eventos</h1>
-      <h1>Eventos</h1>
-      <h1>Eventos</h1>
-      <h1>Eventos</h1>
+    <div className="bg-[#191E28] min-h-screen p-6 text-white">
+      <Toaster />
+      <div className="max-w-7xl mx-auto">
+        <h4 className="text-gray-400 pb-12 pt-8 text-xl">
+          Home / <span className="text-[#6d99fb]">Eventos</span>
+        </h4>
+        <div className="mb-4">
+          <h2 className="text-4xl pb-3">Eventos</h2>
+          <p className="text-gray-400 text-lg">
+            Confira a lista de todos os eventos cadastrados
+          </p>
+        </div>
+
+        <div className="bg-[#10141D] p-4 rounded-xl pt-8">
+          <div className="flex items-center mb-4 pb-5">
+            <div className="flex items-center bg-gray-700 rounded-lg px-3 py-2 w-1/3 relative">
+              <FontAwesomeIcon
+                className="text-gray-400 mr-2 cor-icon-search"
+                icon={faSearch}
+              />
+              <input
+                type="text"
+                placeholder="Pesquise por nome do evento"
+                className="bg-transparent focus:outline-none text-gray-300 w-full"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </div>
+            <a
+              href="/criar-evento"
+              className="ml-auto bg-[#EBF0F9] text-[#10141d] hover:bg-[#D0D8E1] hover:text-[#252d3f] hover:shadow-lg transition duration-300 ease-in-out px-4 py-2 rounded-lg flex items-center text-xl font-semibold"
+            >
+              Adicionar evento
+            </a>
+          </div>
+
+          <div className="overflow-x-auto">
+            <table className="w-full table-auto">
+              <thead>
+                <tr className="bg-[#10141D] text-[#CAD6EC]">
+                  <th className="p-3 text-left">Evento</th>
+                  <th className="p-3 text-left">Tipo</th>
+                  <th className="p-3 text-left">Local associado</th>
+                  <th className="p-3 text-left">Endereço</th>
+                  <th className="p-3 text-left">Portões cadastrados</th>
+                  <th className="p-3 text-left">Data</th>
+                  <th className="p-3"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {Array.isArray(displayedEventos) &&
+                displayedEventos.length > 0 ? (
+                  displayedEventos.map((evento, index) => (
+                    <tr
+                      key={evento.id_evento}
+                      className={`${
+                        index % 2 === 0 ? "bg-[#333B49]" : "bg-[#10141D]"
+                      } text-gray-300`}
+                    >
+                      <td className="p-3">{evento.nome}</td>
+                      <td className="p-3">{evento.tipo}</td>
+                      <td className="p-3">{evento.local?.nome}</td>
+                      <td className="p-3">
+                        {evento.local?.endereco} 
+                      </td>
+                      <td className="p-3">
+                        {evento.local?.portões} 
+                      </td>
+                      <td className="p-3">
+                        {evento.data_evento && formatDate(evento.data_evento)}
+                      </td>
+                      <td className="p-5 text-right relative">
+                        <FontAwesomeIcon
+                          className="cursor-pointer text-gray-400 icon-options"
+                          icon={faEllipsisV}
+                          onClick={() => toggleDropdown(evento.id_evento)}
+                        />
+                        {selectedEvento === evento.id_evento && (
+                          <div className="absolute right-0 mt-2 w-32 bg-[#191E28] rounded-lg shadow-lg z-10">
+                            <ul className="py-1">
+                              <li
+                                className="text-left px-4 py-3 text-gray-300 text-lg font-normal hover:bg-[#2f3746] rounded-t-lg cursor-pointer"
+                                onClick={() => handleEdit(evento.id_evento)} // Call handleEdit when "Edit" is clicked
+                              >
+                                Edit
+                              </li>
+                              <li
+                                className="text-left px-4 py-3 text-gray-300 text-lg font-normal hover:bg-[#2f3746] rounded-b-lg cursor-pointer"
+                                onClick={() => deleteEvento(evento.id_evento)} // Chama a função de deletar
+                              >
+                                Apagar
+                              </li>
+                            </ul>
+                          </div>
+                        )}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={7} className="p-4 text-center text-gray-400">
+                      Nenhum evento encontrado.
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="flex justify-center mt-4">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                className={`px-4 py-2 mx-1 rounded-lg ${
+                  currentPage === index + 1
+                    ? "bg-gray-700 text-gray-300"
+                    : "text-gray-300"
+                }`}
+                onClick={() => changePage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Eventos;
